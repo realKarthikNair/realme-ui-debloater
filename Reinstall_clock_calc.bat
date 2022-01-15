e3997386139435aad1d224ca5a7a681ac4e80fab
Karthik Nair <knairothera@gmail.com>
2022-01-15 17:05:46 +0530

    Add files via upload

--- Reinstall_clock_calc.bat
+++ Reinstall_clock_calc.bat
@@ -1,3 +1,25 @@
+@echo OFF
+set s=---------------------------------------------------------------------------
+set m1= Script made By realkarthiknair
+set m2= Go turn on usb debugging if you haven't did till now and connect your device to pc.
+set m3= Press enter to reinstall stock clock and calc
+echo %m1%
+echo %m3%
+echo %s%
+echo %m2%
+echo %s%
+pause
+adb wait-for-any-device
+adb reconnect
+adb wait-for-any-device
 adb shell pm install-existing com.coloros.alarmclock
 adb shell pm install-existing com.coloros.calculator
 adb shell pm enable com.coloros.calculator
+echo %s%
+echo success
+echo %s%
+echo %m1%
+echo %s%
+echo if you like this work, dont forget to follow realkarthiknair at instagram 
+echo %s%
+pause
--- reinstall_facebook_with_facebook_services.bat.bat
+++ reinstall_facebook_with_facebook_services.bat.bat
@@ -1,4 +1,27 @@
+@echo OFF
+set s=---------------------------------------------------------------------------
+set m1= Script made By realkarthiknair
+set m2= Go turn on usb debugging if you haven't did till now and connect your device to pc.
+set m3= Press enter to reinstall facebook core app
+echo %m1%
+echo %m3%
+echo %s%
+echo %m2%
+echo %s%
+pause
+adb wait-for-any-device
+adb reconnect
+adb wait-for-any-device
 adb shell pm uninstall -k --user 0 com.facebook.katana
 adb shell pm uninstall -k --user 0 com.facebook.system
 adb shell pm uninstall -k --user 0 com.facebook.appmanager
 adb shell pm uninstall -k --user 0 com.facebook.services
+echo %s%
+echo Success
+echo %s%
+echo %m1%
+echo %s%
+echo if you like this work, dont forget to follow realkarthiknair at instagram 
+echo %s%
+pause
+
