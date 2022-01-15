06feeed3ef578d016847ff4eb9e7fe04e0b08431
Karthik Nair <knairothera@gmail.com>
2022-01-15 16:43:27 +0530

    cheers, zuckerberg fans!

---
+++ reinstall_facebook_with_facebook_services.bat.bat
@@ -0,0 +1,4 @@
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.services
