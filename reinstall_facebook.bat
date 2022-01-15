cfbaa3a3c8eb1acc11ec5e0816aff7b0e7a67e7a
Karthik Nair <knairothera@gmail.com>
2022-01-15 16:33:23 +0530

    made for zuckerberg fans

---
+++ reinstall_facebook.bat
@@ -0,0 +1,4 @@
+adb shell pm install-existing -k --user 0 com.facebook.katana
+adb shell pm install-existing -k --user 0 com.facebook.system
+adb shell pm install-existing -k --user 0 com.facebook.appmanager
+adb shell pm install-existing -k --user 0 com.facebook.services
