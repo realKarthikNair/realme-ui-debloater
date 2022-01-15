3dc28b3fe22adb80320a62b07be4c25a6b02bc4a
Karthik Nair <knairothera@gmail.com>
2022-01-15 17:15:24 +0530

    Update reinstall_facebook_with_facebook_services.bat

--- reinstall_facebook_with_facebook_services.bat
+++ reinstall_facebook_with_facebook_services.bat
@@ -2,7 +2,7 @@
 set s=---------------------------------------------------------------------------
 set m1= Script made By realkarthiknair
 set m2= Go turn on usb debugging if you haven't did till now and connect your device to pc.
-set m3= Press enter to reinstall facebook core app
+set m3= Press enter to reinstall facebook core app and services
 echo %m1%
 echo %m3%
 echo %s%
