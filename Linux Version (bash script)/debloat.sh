c892d2cad76704e8a497867b78e7ea691aecf120
realkarthiknair <knairothera@gmail.com>
2022-02-10 02:49:15 +0530

    _

---
+++ Linux Version (bash script)/debloat.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+echo "Do you want to Debloat you Device?"
+read input
+if [[ "$input" == "y" ]] || [[ "$input" == "yes" ]] || [[ "$input" == "Y" ]]; then
+    adb wait-for-any-device
+    adb reconnect
+    adb wait-for-any-device
+    adb shell pm uninstall cn.wps.moffice_eng
+    adb shell pm uninstall com.google.android.apps.podcasts
+    adb shell pm uninstall com.realme.link
+    adb shell pm uninstall com.google.android.apps.youtube.music
+    adb shell pm uninstall com.google.android.apps.tachyon
+    adb shell pm uninstall -k --user 0 com.coloros.systemclone
+    adb shell pm uninstall -k --user 0 com.realmepay.payments
+    adb shell pm uninstall -k --user 0 com.opos.cs
+    adb shell pm uninstall -k --user 0 com.facebook.katana
+    adb shell pm uninstall -k --user 0 com.facebook.system
+    adb shell pm uninstall -k --user 0 com.facebook.appmanager
+    adb shell pm uninstall -k --user 0 com.facebook.services
+    adb shell pm uninstall -k --user 0 com.realme.securitycheck
+    adb shell pm uninstall -k --user 0 com.coloros.phonemanager
+    adb shell pm uninstall -k --user 0 com.coloros.oppomultiapp
+    adb shell pm uninstall -k --user 0 com.coloros.gamespace
+    adb shell pm uninstall -k --user 0 com.heytap.music
+    adb shell pm uninstall -k --user 0 com.heytap.themestore
+    adb shell pm uninstall -k --user 0 com.heytap.pictorial
+    adb shell pm uninstall -k --user 0 com.heytap.habit.analysis
+    adb shell pm uninstall -k --user 0 com.coloros.backuprestore
+    adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
+    adb shell pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+    adb shell pm uninstall -k --user 0 com.coloros.oshare
+    adb shell pm uninstall -k --user 0 com.oppo.quicksearchbox
+    adb shell pm uninstall -k --user 0 com.coloros.gamespaceui
+    adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.files
+    adb shell pm uninstall -k --user 0 com.google.android.youtube
+    adb shell pm uninstall -k --user 0 com.coloros.assistantscreen
+    adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.paisa.user
+    adb shell pm uninstall -k --user 0 com.coloros.videoeditor
+    adb shell pm uninstall -k --user 0 com.coloros.video
+    adb shell pm uninstall -k --user 0 com.heytap.browser
+    adb shell pm uninstall -k --user 0 com.os.docvault
+    adb shell pm uninstall -k --user 0 com.finshell.fin
+    adb shell pm uninstall com.coloros.onekeylockscreen
+    adb shell pm uninstall com.realmecomm.app
+    adb shell pm uninstall com.heytap.quickgame
+    adb shell pm uninstall -k --user 0 com.heytap.cloud
+    adb shell pm uninstall -k --user 0 com.heytap.usercenter
+    adb shell pm uninstall com.realmestore.app
+    adb shell pm uninstall com.google.android.videos
+    adb shell pm uninstall -k --user 0 com.redteamobile.roaming
+    adb shell pm disable-user com.android.fmradio
+    adb shell pm disable-user com.nearme.statistics.rom
+    echo "Your realme device has been Debloated"
+else
+    echo "Abort."
+fi
+
---
+++ Undo_all_actions.bat
@@ -0,0 +1,67 @@
+@echo OFF
+set s=---------------------------------------------------------------------------
+set m1= Script made By realkarthiknair
+set m2= Go turn on usb debugging if you haven't did till now and connect your device to pc.
+set m3= you wont face any issues 99.69% of times, but still take backup of really important files
+echo %m1%
+echo %m3%
+echo %s%
+echo %m2%
+echo %s%
+pause
+adb wait-for-any-device
+adb reconnect
+adb wait-for-any-device
+adb shell cmd package install-existing cn.wps.moffice_eng
+adb shell cmd package install-existing com.google.android.apps.podcasts
+adb shell cmd package install-existing com.realme.link
+adb shell cmd package install-existing com.google.android.apps.youtube.music
+adb shell cmd package install-existing com.google.android.apps.tachyon
+adb shell cmd package install-existing com.coloros.systemclone
+adb shell cmd package install-existing com.realmepay.payments
+adb shell cmd package install-existing com.opos.cs
+adb shell cmd package install-existing com.facebook.katana
+adb shell cmd package install-existing com.facebook.system
+adb shell cmd package install-existing com.facebook.appmanager
+adb shell cmd package install-existing com.facebook.services
+adb shell cmd package install-existing com.realme.securitycheck
+adb shell cmd package install-existing com.coloros.phonemanager
+adb shell cmd package install-existing com.coloros.oppomultiapp
+adb shell cmd package install-existing com.coloros.gamespace
+adb shell cmd package install-existing com.heytap.music
+adb shell cmd package install-existing com.heytap.themestore
+adb shell cmd package install-existing com.heytap.pictorial
+adb shell cmd package install-existing com.heytap.habit.analysis
+adb shell cmd package install-existing com.coloros.backuprestore
+adb shell cmd package install-existing com.google.android.apps.magazines
+adb shell cmd package install-existing com.google.android.apps.subscriptions.red
+adb shell cmd package install-existing com.coloros.oshare
+adb shell cmd package install-existing com.oppo.quicksearchbox
+adb shell cmd package install-existing com.coloros.gamespaceui
+adb shell cmd package install-existing com.google.android.apps.nbu.files
+adb shell cmd package install-existing com.google.android.youtube
+adb shell cmd package install-existing com.coloros.assistantscreen
+adb shell cmd package install-existing com.google.android.apps.nbu.paisa.user
+adb shell cmd package install-existing com.coloros.videoeditor
+adb shell cmd package install-existing com.coloros.video
+adb shell cmd package install-existing com.heytap.browser
+adb shell cmd package install-existing com.os.docvault
+adb shell cmd package install-existing com.finshell.fin
+adb shell cmd package install-existing com.coloros.onekeylockscreen
+adb shell cmd package install-existing com.realmecomm.app
+adb shell cmd package install-existing com.heytap.quickgame
+adb shell cmd package install-existing com.heytap.cloud
+adb shell cmd package install-existing com.heytap.usercenter
+adb shell cmd package install-existing com.realmestore.app
+adb shell cmd package install-existing com.google.android.videos
+adb shell cmd package install-existing com.redteamobile.roaming
+adb shell pm enable com.android.fmradio
+adb shell pm enable com.nearme.statistics.rom
+echo %s%
+echo your device packages has been restored
+echo %s%
+echo %m1%
+echo %s%
+echo if you like this work, dont forget to follow realkarthiknair at instagram 
+echo %s%
+pause
