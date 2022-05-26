ff537b6c527bc730e1e12f08b68f681689d44c53
Karthik Nair <knairothera@gmail.com>
2022-05-26 13:03:23 +0530

    Create reinstal_gamespace.bat

---
+++ reinstal_gamespace.bat
@@ -0,0 +1,24 @@
+@echo OFF
+set s=---------------------------------------------------------------------------
+set m1= Script made By realkarthiknair
+set m2= Go turn on usb debugging if you haven't did till now and connect your device to pc.
+set m3= Press enter to reinstall Game Space
+echo %m1%
+echo %m3%
+echo %s%
+echo %m2%
+echo %s%
+pause
+adb wait-for-any-device
+adb reconnect
+adb wait-for-any-device
+adb shell pm install-existing -k --user 0 com.coloros.gamespace
+adb shell pm install-existing -k --user 0 com.coloros.gamespaceui
+echo %s%
+echo success
+echo %s%
+echo %m1%
+echo %s%
+echo if you like this work, dont forget to follow realkarthiknair at instagram 
+echo %s%
+pause
