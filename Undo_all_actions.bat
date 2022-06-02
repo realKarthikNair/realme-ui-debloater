c0ebce44c30ada3bd8abe1a89225a037a79d4bd8
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-06-02 15:56:09 +0530

    Update Undo_all_actions.bat

--- Undo_all_actions.bat
+++ Undo_all_actions.bat
@@ -18,6 +18,12 @@ adb shell cmd package install-existing com.google.android.apps.docs
 adb shell cmd package install-existing com.google.android.keep
 adb shell cmd package install-existing com.google.android.apps.photos
 adb shell cmd package install-existing com.google.android.projection.gearhead
+adb shell cmd package install-existing com.google.android.printservice.recommendation
+adb shell cmd package install-existing com.google.ar.core
+adb shell cmd package install-existing com.google.android.feedback 
+adb shell cmd package install-existing com.coloros.childrenspace
+adb shell cmd package install-existing com.coloros.compass2
+adb shell cmd package install-existing com.coloros.logkit
 adb shell cmd package install-existing cn.wps.moffice_eng
 adb shell cmd package install-existing com.google.android.apps.podcasts
 adb shell cmd package install-existing com.realme.link
